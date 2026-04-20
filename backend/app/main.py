4d6c1205a99e2f8eb3bbe8aeee61364292b83b16
Samudra Uduwaka <samudrauduwaka@gmail.com>
2026-04-20 21:09:23 +0530

    Add Link Quality entity to init model and add router to main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,7 +4,7 @@ import asyncio
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from typing import List, Optional
-from app.routers import topology, meshtastic,texting
+from app.routers import topology, meshtastic, texting, link_quality
 
 from app.services.startup_functions.state import (
   get_visible_entries,
@@ -47,6 +47,7 @@ app.add_middleware(
 app.include_router(topology.router)
 app.include_router(meshtastic.router)
 app.include_router(texting.router)
+app.include_router(link_quality.router)
 
 app.include_router(sdn_serial_router)
 app.include_router(route_table_router)
